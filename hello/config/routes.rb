85c6880a4cfe934c0abb08ca81196933f10ac7f8
Stan Lee <stan@buildzoom.com>
2016-02-13 21:33:48 -0800

    finish static pages

--- hello/config/routes.rb
+++ hello/config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
+  # You can have the root of your site routed with "root"
+  #root 'application#hello'
+  # root "users#index"
+  #root "microposts#index"
+
+
+  # get 'static_pages/home'
+  root "static_pages#home"
   get 'static_pages/help'
 
   resources :microposts
@@ -8,10 +15,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-   #root 'application#hello'
-   root "users#index"
-#   root "microposts#index"
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

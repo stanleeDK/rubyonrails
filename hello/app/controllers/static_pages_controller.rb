cf67d0c2cf8407c7742c83683da18dc293478fd4
Stan Lee <stan@buildzoom.com>
2016-03-12 15:22:41 -0800

    learning how routes work

--- hello/app/controllers/static_pages_controller.rb
+++ hello/app/controllers/static_pages_controller.rb
@@ -1,8 +1,14 @@
 class StaticPagesController < ApplicationController
+ 
   def home
+  	@message = "A message defined inside the controller and sent to the view"
   end
 
   def help
-  	@message = "How can we help you?"
+  	@message = "A message defined inside the controller and send to the view"
+  end
+  
+  def contactus
+  	@message = "A message defined inside the controller and send to the view"
   end
 end
